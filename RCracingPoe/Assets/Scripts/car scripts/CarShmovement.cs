5681935d4982349490d469847e7227247098f26d
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2022-09-21 19:45:33 +0200

    car
    Fixed Car Movement

---
+++ RCracingPoe/Assets/Scripts/car scripts/CarShmovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarShmovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public Vector3 Herspers;
+
+    public Vector3 breaks;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddRelativeForce(-15f, 0, 0, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddRelativeForce(30f, 0, 0, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(0, 0, -0.05f, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Rotate(0, 0, 0.05f, Space.Self);
+        }
+    }
+}
