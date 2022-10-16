7ff8c72e282790b3419729fc72ac7f3c4cfee97b
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2022-10-16 18:16:23 +0200

    linkedList
    started linked list

---
+++ RCracingPoe/Assets/Scripts/CPUdrivers/linkList.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class linkList : MonoBehaviour
+{
+    LinkedList<Vector3> trackPoints = new LinkedList<Vector3>();
+
+    trackPoints..AddLast();
+}
