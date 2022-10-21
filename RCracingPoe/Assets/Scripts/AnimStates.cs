9291bf9cb484c2c2115c0029d265dcd36bdb3915
ChazCrypto <chazrichardson50@gmail.com>
2022-10-21 19:34:14 +0200

    New Functions
    .

--- RCracingPoe/Assets/Scripts/AnimStates.cs
+++ RCracingPoe/Assets/Scripts/AnimStates.cs
@@ -5,38 +5,35 @@ using UnityEngine;
 namespace Spectator.Animation
 {
 
-
     public class AnimStates : StateMachineBehaviour
     {
-        [SerializeField] private Animator anim;
+        //[SerializeField] private Animator anim;
         private float StartAnim;
         private float endAnim;
-        private float lastPosition = 5f;
+        private float lastPosition = 2f;
         private float ChangeState = float.PositiveInfinity;
-        private int AnimPar;
-
+        private int HashPar;
 
         public override void OnStateEnter(Animator anim, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            AnimPar = Animator.StringToHash("Position");
-            base.OnStateEnter(anim, stateInfo, layerIndex);
+            HashPar = Animator.StringToHash("Position");
+            base.OnStateEnter(anim, stateInfo, layerIndex); 
         }
 
         public override void OnStateUpdate(Animator anim, AnimatorStateInfo stateInfo, int Index)
         {
-
-            base.OnStateUpdate(anim, stateInfo, Index);
-
-            anim.SetFloat(AnimPar, Mathf.Lerp(StartAnim, endAnim, lastPosition / ChangeState));
+            
             lastPosition += Time.deltaTime;
             if (lastPosition >= ChangeState)
             {
-                StartAnim = anim.GetFloat(AnimPar);
+                StartAnim = anim.GetFloat(HashPar);
                 endAnim = Random.Range(0f, 1f);
                 lastPosition = 0;
             }
 
+             anim.SetFloat(HashPar, Mathf.Lerp(StartAnim, endAnim, lastPosition / ChangeState));
 
+            base.OnStateUpdate(anim, stateInfo, Index);
         }
     }
 }
